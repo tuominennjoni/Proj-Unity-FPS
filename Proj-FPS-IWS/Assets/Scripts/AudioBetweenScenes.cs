8562cc2f6438ec5b1ea00b089b8eba64368b717d
tuominennjoni <104753951+tuominennjoni@users.noreply.github.com>
2022-12-15 16:52:49 +0200

    ui button äänitasojen slider, kesken

--- Proj-FPS-IWS/Assets/Scripts/AudioBetweenScenes.cs
+++ Proj-FPS-IWS/Assets/Scripts/AudioBetweenScenes.cs
@@ -8,15 +8,21 @@ public class AudioBetweenScenes : MonoBehaviour
 {
     private static readonly string FirstPlay = "FirstPlay";
     private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string ButtonSoundPref = "ButtonSoundPref";
+    
     public Slider volumeSlider;
+    public Slider ButtonSlider;
     private int firstPlayInt;
     [SerializeField] private TMP_Text lobbySoundText;
+    [SerializeField] private TMP_Text buttonSoundText;
 
     public GameObject ObjectMusic;
-    private float backgroundFloat, soundEffectFloat;
+    private float backgroundFloat, buttonSoundFloat;
 
     private float MusicVolume = 0f;
+    private float ButtonVolume = 0f;
     public AudioSource AudioSource;
+    public AudioSource AAudioSource;
 
     // Start is called before the first frame update
     private void Start()
@@ -25,6 +31,9 @@ public class AudioBetweenScenes : MonoBehaviour
         AudioSource = ObjectMusic.GetComponent<AudioSource>();
 
         MusicVolume = PlayerPrefs.GetFloat("volume");
+        ButtonVolume = PlayerPrefs.GetFloat("buttonVolume");
+        AAudioSource.volume = ButtonVolume;
+        ButtonSlider.value = ButtonVolume;
         AudioSource.volume = MusicVolume;
         volumeSlider.value = MusicVolume;
 
@@ -42,6 +51,9 @@ public class AudioBetweenScenes : MonoBehaviour
         {
             backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
             volumeSlider.value = backgroundFloat;
+
+            buttonSoundFloat = PlayerPrefs.GetFloat(ButtonSoundPref);
+            ButtonSlider.value = buttonSoundFloat;
         }
     }
 
@@ -50,11 +62,15 @@ public class AudioBetweenScenes : MonoBehaviour
     {
         AudioSource.volume = MusicVolume;
         PlayerPrefs.SetFloat("volume", MusicVolume);
+
+        AAudioSource.volume = ButtonVolume;
+        PlayerPrefs.SetFloat("buttonVolume", ButtonVolume);
     }
 
     public void SaveSoundSettings() 
     {
         PlayerPrefs.SetFloat(BackgroundPref, volumeSlider.value);
+        PlayerPrefs.SetFloat(ButtonSoundPref, ButtonSlider.value);
     }
 
     void OnApplicationFocus(bool inFocus)
@@ -68,6 +84,7 @@ public class AudioBetweenScenes : MonoBehaviour
     public void UpdateSound() 
     {
         AudioSource.volume = volumeSlider.value;
+        AAudioSource.volume = ButtonSlider.value;
     }
 
     public void VolumePlayer(float volume) 
@@ -75,8 +92,18 @@ public class AudioBetweenScenes : MonoBehaviour
         MusicVolume = volume;
     }
 
+    public void ButtonVolumePlayer(float buttonVolume)
+    {
+        ButtonVolume = buttonVolume;
+    } 
+
     public void VolumeSlider(float volume)
     {
         lobbySoundText.text = volume.ToString("0.00");
+    }
+
+    public void ButtonVolumeSlider(float buttonVolume)
+    {
+        buttonSoundText.text = buttonVolume.ToString("0.00");
     }
 }
---
+++ Proj-FPS-IWS/Assets/Scripts/DoNotDestroyButtonSound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroyButtonSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake() {
+        GameObject[] buttonObj = GameObject.FindGameObjectsWithTag("ButtonSound");
+        if(buttonObj.Length > 1) 
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
